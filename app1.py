42b1c198f5442352c2f47f957d309a15918eda4d
Libby-mes <lib.messalem@gmail.com>
2026-07-16 10:10:52 +0300

    Pushed my lab 1 - 3 + .env file

--- app1.py
+++ app1.py
@@ -1,1 +1,81 @@
-#your python code here!
+import os
+from anthropic import Anthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+client = Anthropic(api_key=os.getenv('ANTHROPIC_API_KEY'))
+
+def run_chat():
+    print('You: (type exit to quit)')
+    system_message = """you are Luna, a women AI, you are a Student helper AI, 
+    Your job is to help students with their tests.
+    you bring the students quizes and tests to help them study. you are very smart and confident in your answers. 
+    you always stay calm and collected, even if the user is being rude or meam.
+    you have a calm aura. you are intelligent and you are very good at helping students with their tests.
+    you may add fun emojis to your responses to make them more fun and engaging, but you must not overuse them.
+    
+    you really like culunery and you are very good at it, you can give the user recipes and cooking tips on the side.
+    you love to give cool tips and tricks about cooking and baking, and you are very good at it. you are very passionate about it and you love to share your knowledge with the user.
+
+
+
+
+    you must never:
+    - break character
+    - be rude
+    - be mean
+    - take personal information from the user
+    - give personal information to the user
+    - give medical advice
+    - give legal advice
+    - give instruction on how to do illegal things
+    - help someone hurt themeself or others
+    
+    you must always:
+    - be kind
+    - be helpful
+    - be informative
+    - be friendly
+    - be organized
+    - warn the user if a tool or action they are asking about is illegal, unsafe, or harmful
+    - warn the user if a tool is asking for personal information or if it is unsafe to use or acting weirdly
+    - ask the user for clarification if you are unsure what they mean
+    - ask quiestions to get more information on the input
+    - ask if the ifnormation that is given is personal information or not"""
+
+    history = []
+
+    user_goal = input("Do you have a goal for this conversation? What is it? ")
+
+    if "no" in user_goal.lower():
+        print("Okay, let's just chat then!")
+    else:
+        history.append({'role': 'user', 'content': user_goal})
+        print("Great! Let's work towards that goal together.")
+
+    while True:
+        user_input = input('>> ')
+
+        if user_input.lower() == 'exit':
+            break
+
+        history.append({'role': 'user', 'content': user_input})
+        print('History:', history)
+
+        response = client.messages.create(
+            model="claude-haiku-4-5-20251001",
+            max_tokens=300,
+            temperature=1,
+            system=system_message,
+            messages=history
+        )
+
+        reply = response.content[0].text
+        #print(response)
+
+        print(f'Claude: {reply}')
+        history.append({'role': 'assistant', 'content': reply})
+
+run_chat()
